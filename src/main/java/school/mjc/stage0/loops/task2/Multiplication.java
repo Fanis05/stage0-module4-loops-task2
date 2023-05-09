7a3fc116a812d5ade34eece7b7fda5073a1de017
Fanis <mrduck597@gmail.com>
2023-05-09 15:27:01 +0500

    test1

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int counter = 0;
+        while (counter * counter <= multiplyByAndToInclusive * multiplyByAndToInclusive) {
+            System.out.println(multiplyByAndToInclusive * counter);
+            counter++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int number = 0;
+        int count = 0;
+        int devider = 1;
+        while (number <= printToInclusive){
+            count = 0;
+            devider = 1;
+            while(devider <=number){
+                if (number % devider == 0){
+                    count++;
+                    devider++;
+                }
+                if (count == 2){
+                    System.out.println(number);
+                }
+            }
+            number ++;
+        }
     }
 }
